27ac061cfe579a82c6f5931dc794eebdeaa4e72d
Efrata <efratahabtu72@gmail.com>
2025-09-14 20:20:17 +0300

    Add script to print 'C is fun' x times using loop and input validation

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,10 +2,12 @@
 
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x < 0) {
+if (isNaN(x) || x <= 0) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
