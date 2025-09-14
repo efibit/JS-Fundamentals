fcb6cc4c714c975f06e95a35849e9507171f4a68
Efrata <efratahabtu72@gmail.com>
2025-09-14 20:24:02 +0300

    Add script to print square of X characters based on input size

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+// File: 8-square.js
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size) || size <= 0) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  while (i < size) {
+    console.log('X'.repeat(size));
+    i++;
+  }
+}
